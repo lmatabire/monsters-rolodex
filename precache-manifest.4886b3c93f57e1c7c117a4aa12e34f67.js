56aabc3d6de958a6b984443385e483ec285808ee
Lee Matabire <Matabire23@gmail.com>
2020-05-05 12:11:05 -0500

    Updates

---
+++ precache-manifest.4886b3c93f57e1c7c117a4aa12e34f67.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2253465333b1ad3fa2c3098ee8f11e8",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "a5096d6f239342b393a6",
+    "url": "/monsters-rolodex/static/css/main.ebfda56c.chunk.css"
+  },
+  {
+    "revision": "53a8573b68da4696eb47",
+    "url": "/monsters-rolodex/static/js/2.080c7e48.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.080c7e48.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5096d6f239342b393a6",
+    "url": "/monsters-rolodex/static/js/main.506d08ea.chunk.js"
+  },
+  {
+    "revision": "533b0f57627591e21c0f",
+    "url": "/monsters-rolodex/static/js/runtime-main.aeffa41f.js"
+  }
+]);
